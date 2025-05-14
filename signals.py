56ca1ee9ab04043daa265647d1ff67f8c9c86769
oleksandr2107 <solomiakarlicuk@gmail.com>
2025-05-14 21:49:10 +0200

    Update signals.py

--- signals.py
+++ signals.py
@@ -1,7 +1,9 @@
 def get_bybit_open_interest(threshold=1.0):
-    # тут код перевірки через Bybit API
-    return []
+    return [
+        {'pair': 'BTCUSDT', 'signal': f'LONG (+{threshold}%)', 'price': '$65,000', 'link': 'https://www.coinglass.com/tv/BTCUSDT'}
+    ]
 
 def get_price_spike_signals(threshold=1.2):
-    # тут код перевірки Binance
-    return []
+    return [
+        {'pair': 'ETHUSDT', 'signal': f'SHORT (-{threshold}%)', 'price': '$2,900', 'link': 'https://www.coinglass.com/tv/ETHUSDT'}
+    ]
